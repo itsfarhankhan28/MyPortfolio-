9a0f06800dd507fcbd7098be6c9d45ca11994374
itsfarhankhan28 <famannat@gmail.com>
2023-03-21 20:07:11 +0530

    added a debug screen and different screen sizes in tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,28 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
+    debugScreens: {
+      position: ['bottom', 'left'],
+      selector: '.debug-screens',
+      style: {
+        backgroundColor: 'black',
+        color: 'white',
+        fontSize: '15px',
+      },
+    },
+    screens: {
+      xxsm: '320px',
+      xsm: '425px',
+      mobile: '425px',
+      sm: '640px',
+      tablet: '640px',
+      md: '768px',
+      lg: '1024px',
+      laptop: '1024px',
+      xl: '1440px',
+      desktop: '1280px',
+      '2xl': '1536px',
+    },
     extend: {
       fontFamily:{
         herofont:['Prompt']
