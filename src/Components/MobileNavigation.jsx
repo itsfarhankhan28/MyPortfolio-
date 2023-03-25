a915022b11889f60a46bb1bc721358729db2d649
itsfarhankhan28 <famannat@gmail.com>
2023-03-25 16:44:31 +0530

    build(MobileNavigation):Build Mobile responsive navbar

---
+++ src/Components/MobileNavigation.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import NavLinks from './NavLinks'
+import {GiHamburgerMenu} from 'react-icons/gi'
+import {AiOutlineClose} from 'react-icons/ai'
+
+const MobileNavigation = () => {
+
+  const [isOpen , setisOpen] = useState(false)
+
+  const HamburgerMenu = <GiHamburgerMenu 
+                        color='black' 
+                        size='40px' 
+                        className='absolute right-[5%] cursor-pointer p-3 h-[50px] w-[50px]' 
+                        onClick={()=>setisOpen(!isOpen)}/>
+
+  const CloseIcon = <AiOutlineClose 
+                    color='black' 
+                    size='40px' 
+                    className='absolute right-[5%] cursor-pointer z-20 p-3 h-[50px] w-[50px]' 
+                    onClick={()=>setisOpen(!isOpen)}/>
+
+  return (
+    <>
+      <nav className='md:hidden bg-blue-300 flex items-center'>
+        {isOpen ? CloseIcon : HamburgerMenu}
+        {isOpen && <NavLinks/>}
+      </nav>
+    </>
+  )
+}
+
+export default MobileNavigation
