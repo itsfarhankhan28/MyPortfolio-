923bb329681f19d65c2161cf805503b2ae610d53
itsfarhankhan28 <famannat@gmail.com>
2023-03-25 20:07:24 +0530

    Style(Navbar):Changed position of navlinks

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,10 +5,10 @@ import MobileNavigation from './MobileNavigation'
 const Navbar = () => {
   return (
     <>
-     <div className='fixed flex p-5 w-[100vw] justify-center items-center'>
-        <Navigation/>
-        <MobileNavigation/>
-     </div>
+      <div className='fixed flex p-5 w-[100vw] justify-end items-center'>
+          <Navigation/>
+          <MobileNavigation/>
+      </div>
     </>
   )
 }
