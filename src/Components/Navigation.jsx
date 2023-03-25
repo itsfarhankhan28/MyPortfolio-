cdd942dbcb7df1030795caa220453ca7f5691a2d
itsfarhankhan28 <famannat@gmail.com>
2023-03-25 20:09:58 +0530

    Style(Navigation):Added Portfolio Icon to Navigation

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -4,9 +4,14 @@ import NavLinks from './NavLinks'
 const Navigation = () => {
   return (
     <>
+    <div className='flex justify-items items-center'>
+      <div className='absolute left-[3%]'>
+        <h1 className='text-3xl font-herofont'>Fk</h1>
+      </div>
       <nav className='md:inline-block xxsm:hidden'>
         <NavLinks/>
       </nav>
+    </div>
     </>
   )
 }
