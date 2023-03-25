227bc0755123c2d5826853b2539c85053d89d1de
itsfarhankhan28 <famannat@gmail.com>
2023-03-25 16:50:20 +0530

    build(Navigation):Build Navigation

---
+++ src/Components/Navigation.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavLinks from './NavLinks'
+
+const Navigation = () => {
+  return (
+    <>
+      <nav className='md:inline-block xxsm:hidden'>
+        <NavLinks/>
+      </nav>
+    </>
+  )
+}
+
+export default Navigation
