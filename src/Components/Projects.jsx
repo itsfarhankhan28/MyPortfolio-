c1069e393f5f3cae0437bc2f20e03ba7e49dcb02
itsfarhankhan28 <famannat@gmail.com>
2023-03-30 15:28:02 +0530

    doc(Projects):Created Project Component

---
+++ src/Components/Projects.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import Carousel from './Carousel'
+import { v4 as uuidv4 } from "uuid";
+import Card from './ProjectCards';
+// import ProjectImg1 from '../Assets/Project1 Demo.png'
+
+const Projects = () => {
+
+  let cards = [
+    {
+      key:uuidv4(),
+      content:(
+        <Card serialNo="01" projectInfo="The purpose of this website is to provide users with a collection of recipes , cooking tips and food related information . The goal is to inspire and assist users in the kitchen . The technology used is MERN stack" image="../Assets/Project1 Demo.png"/>
+      )
+    },
+    {
+      key:uuidv4(),
+      content:(
+        <Card serialNo="02"/>
+      )
+    },
+    {
+      key:uuidv4(),
+      content:(
+        <Card serialNo="03"/>
+      )
+    },
+    {
+      key:uuidv4(),
+      content:(
+        <Card serialNo="04"/>
+      )
+    },
+    {
+      key:uuidv4(),
+      content:(
+        <Card serialNo="05"/>
+      )
+    },
+  ]
+
+  return (
+    <>
+    <div className='overflow-hidden bg-custom-brown'>
+      {/* <h1 className='font-herofont text-3xl text-center pt-10'>My Work</h1> */}
+      <div className='flex justify-center items-center h-[100vh]'>
+        <Carousel
+          cards={cards}
+          height="500px"
+          width="30%"
+          margin="0 auto"
+          offset={2}
+          showArrows={false}
+        />
+      </div>
+      </div>
+    </>
+  )
+}
+
+export default Projects
