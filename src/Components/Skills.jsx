fcf4389d9da63712b80919a955a733d022892abb
itsfarhankhan28 <famannat@gmail.com>
2023-03-31 15:22:40 +0530

    doc(Skills):Created Skills component

---
+++ src/Components/Skills.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Skills = () => {
+  return (
+    <>
+     <div className='h-screen bg-blue-300'>
+        <h1>My Skills</h1>
+     </div> 
+    </>
+  )
+}
+
+export default Skills
