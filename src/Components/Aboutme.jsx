c0031cbfd2caf876e39332dc93d379b82716876c
itsfarhankhan28 <famannat@gmail.com>
2023-03-31 15:19:05 +0530

    doc(Aboutme):Created About Me component

---
+++ src/Components/Aboutme.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Aboutme = () => {
+  return (
+    <>
+        <div className='h-screen bg-green-200'>
+            <h1>About Me</h1>
+        </div> 
+    </>
+  )
+}
+
+export default Aboutme
