c94b25044b50c26f5a386e8750a2fb608462e20d
itsfarhankhan28 <famannat@gmail.com>
2023-04-02 15:57:33 +0530

    feat(Button):Added source code link to button

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
+// import { useNavigate } from 'react-router'
 
 const Button = (props) => {
+
   return (
     <>
+    <a href="https://www.sitepoint.com/github-profile-readme/" target='_blank'>
       <button className='my-3 border-2 border-solid border-black p-2 rounded-xl'>
         {props.btnName}
       </button>
+    </a>
     </>
   )
 }
