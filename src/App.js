9b0cc9e0565f76c817aaad137a9771c2c3e32faa
itsfarhankhan28 <famannat@gmail.com>
2023-04-02 15:52:07 +0530

    refactor(App):Commented smooth scroll component

--- src/App.js
+++ src/App.js
@@ -4,19 +4,12 @@ import Herosection from './Components/Herosection';
 import Navbar from './Components/Navbar';
 import Projects from './Components/Projects';
 import Skills from './Components/Skills';
-import { ScrollerMotion } from 'scroller-motion';
+// import { ScrollerMotion } from 'scroller-motion';
 // import SmoothScroll from './Components/SmoothScroll';
 
 function App() {
   return (
     <>
-    <ScrollerMotion 
-    scale={1.5}
-    spring={{
-      damping: 50,
-      mass: 10,
-      stiffness: 100
-    }}>
       <Navbar/>
       <Herosection heading1="Hello" heading2="I am Farhan Khan"
       para1="A passionate and motivated web" para2="developer . I am dedicated to creating"
@@ -24,7 +17,6 @@ function App() {
       <Projects/>
       <Skills/>
       <Aboutme/>
-    </ScrollerMotion>
     </>
   );
 }
