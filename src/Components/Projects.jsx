a1fb8851929caafb93e65b7d0a3f170eb0cb79c5
itsfarhankhan28 <famannat@gmail.com>
2023-04-02 15:58:59 +0530

    Style(Projects):Added style

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -3,6 +3,8 @@ import Carousel from './Carousel'
 import { v4 as uuidv4 } from "uuid";
 import Card from './ProjectCards';
 import ProjectImg1 from '../Assets/Project1 Demo.png'
+import ProjectImg2 from '../Assets/Project2 Demo.png'
+
 
 const Projects = () => {
 
@@ -10,13 +12,13 @@ const Projects = () => {
     {
       key:uuidv4(),
       content:(
-        <Card serialNo="01" projectInfo="The purpose of this website is to provide users with a collection of recipes , cooking tips and food related information . The goal is to inspire and assist users in the kitchen.The technology used is MERN stack" image={ProjectImg1} projectTitle="All About Recipe"/>
+        <Card serialNo="01" projectInfo="The purpose of this website is to provide users with a collection of recipes , cooking tips and food related information . The goal is to inspire and assist users in the kitchen.The technology used is MERN stack" image={ProjectImg1} projectTitle="All About Recipe" link="https://www.sitepoint.com/github-profile-readme/"/>
       )
     },
     {
       key:uuidv4(),
       content:(
-        <Card serialNo="02"/>
+        <Card serialNo="02" projectInfo="The purpose of this website is to provide users with a collection of recipes , cooking tips and food related information . The goal is to inspire and assist users in the kitchen.The technology used is MERN stack" image={ProjectImg2} projectTitle="Clone Website"/>
       )
     },
     {
@@ -42,8 +44,8 @@ const Projects = () => {
   return (
     <>
     <div className='overflow-hidden bg-custom-brown'>
-      <div className='flex justify-center items-center flex-col h-[100vh]'>
-      <h1 className='font-herofont text-3xl font-semibold text-center pt-10'>My Work</h1>
+      <div className='flex justify-center items-center flex-col gap-5 h-[100vh]'>
+      <h1 className='font-herofont text-[35px] font-semibold text-center'>My Work</h1>
         <Carousel
           cards={cards}
           height="500px"
