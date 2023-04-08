fdf5b4559ca67326d4cfa5cd7cffa2de0b260c81
itsfarhankhan28 <famannat@gmail.com>
2023-04-08 13:50:36 +0530

    feat(Skills): added all the new components to Skill section

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import ProgrammingLang from './ProgrammingLang'
+import WebDevelopment from './WebDevelopment'
+import Tools from './Tools'
+import Others from './Others'
 
 const Skills = () => {
   return (
     <>
-     <div className='h-screen bg-blue-300'>
-        <h1>My Skills</h1>
+     <div className='h-screen bg-custom-brown flex flex-col'>
+        <h1 className='text-center pt-5 font-herofont text-[35px] font-semibold'>My Skills</h1>
+        <div className='w-[1350px] mx-auto h-auto grid grid-cols-4 gap-10 mt-5'>
+            <ProgrammingLang/>
+            <WebDevelopment/>
+            <Tools/>
+            <Others/>
+        </div>
      </div> 
     </>
   )
