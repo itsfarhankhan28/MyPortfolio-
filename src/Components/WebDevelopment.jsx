af2218c5ec38920a9a131293eb3156e7ea8ab933
itsfarhankhan28 <famannat@gmail.com>
2023-04-08 13:48:58 +0530

    feat(WebDevelopment): created a new component WebDevelopment

---
+++ src/Components/WebDevelopment.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const WebDevelopment = () => {
+  return (
+    <>
+        <div className=''>
+            <h1 className='text-center font-herofont text-xl font-semibold'>Web Development</h1>
+            <div className='px-2 py-10 grid grid-cols-1 gap-8'>
+                <div>
+                    <img src="https://skillicons.dev/icons?i=html" alt="" />
+                </div>
+                <div>
+                    <img src="https://skillicons.dev/icons?i=css" alt="" />
+                </div>
+                <div>
+                    <img src="https://skillicons.dev/icons?i=expressjs" alt="" />
+                </div>
+                <div>
+                    <img src="https://skillicons.dev/icons?i=tailwind" alt="" />
+                </div>
+                <div>
+                    <img src="https://skillicons.dev/icons?i=nodejs" alt="" />
+                </div>
+                <div>
+                    <img src="https://skillicons.dev/icons?i=react" alt="" />
+                </div>
+            </div>
+        </div> 
+    </>
+  )
+}
+
+export default WebDevelopment
