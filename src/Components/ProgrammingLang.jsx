fcab655531d687e0d5093c0e9fce36b2bc099e8b
itsfarhankhan28 <famannat@gmail.com>
2023-04-08 13:45:42 +0530

    feat(ProgrammingLang): created a new component ProgrammingLang

---
+++ src/Components/ProgrammingLang.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const ProgrammingLang = () => {
+  return (
+    <>
+        <div className=''>
+            <h1 className='text-center font-herofont text-xl font-semibold'>Programming Languages</h1>
+            <div className='px-2 py-10 grid grid-cols-1 gap-8'>
+                <div>
+                    <img src="https://skillicons.dev/icons?i=c" alt="" />
+                </div>
+                <div>
+                    <img src="https://skillicons.dev/icons?i=python" alt="" />
+                </div>
+                <div>
+                    <img src="https://skillicons.dev/icons?i=java" alt="" />
+                </div>
+                <div>
+                    <img src="https://skillicons.dev/icons?i=javascript" alt="" />
+                </div>
+                <div>
+                    <img src="https://skillicons.dev/icons?i=typescript" alt="" />
+                </div>
+            </div>
+        </div> 
+    </>
+  )
+}
+
+export default ProgrammingLang
