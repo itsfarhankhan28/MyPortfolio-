01c63a788b237c7a6ed6c7d976a462e83d017f70
itsfarhankhan28 <famannat@gmail.com>
2023-04-08 13:43:51 +0530

    feat(Others): created a new component Others

---
+++ src/Components/Others.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Others = () => {
+  return (
+    <>
+        <div className=''>
+            <h1 className='text-center font-herofont text-xl font-semibold'>Others</h1>
+            <div className='px-2 py-10 grid grid-cols-1 gap-8'>
+                <div>
+                    <img src="https://skillicons.dev/icons?i=figma" alt="" />
+                </div>
+                <div>
+                    <img src="https://skillicons.dev/icons?i=webflow" alt="" />
+                </div>
+                <div>
+                    <img src="https://skillicons.dev/icons?i=mui" alt="" />
+                </div>
+                <div>
+                    <img src="https://skillicons.dev/icons?i=markdown" alt="" />
+                </div>
+                <div>
+                    <img src="https://skillicons.dev/icons?i=mongodb" alt="" />
+                </div>
+            </div>
+        </div> 
+    </>
+  )
+}
+
+export default Others
