3877faf9e86f537b3653a28f698d7eab85319259
itsfarhankhan28 <famannat@gmail.com>
2023-04-08 13:47:29 +0530

    feat(Tools): created a new component Tools

---
+++ src/Components/Tools.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Tools = () => {
+  return (
+    <>
+        <div className=''>
+            <h1 className='text-center font-herofont text-xl font-semibold'>Tools</h1>
+            <div className='px-2 py-10 grid grid-cols-1 gap-8'>
+                <div>
+                    <img src="https://skillicons.dev/icons?i=vscode" alt="" />
+                </div>
+                <div>
+                    <img src="https://skillicons.dev/icons?i=postman" alt="" />
+                </div>
+                <div>
+                    <img src="https://skillicons.dev/icons?i=git" alt="" />
+                </div>
+            </div>
+        </div>  
+    </>
+  )
+}
+
+export default Tools
