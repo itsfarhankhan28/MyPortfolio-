8283b06f517f20ab3d371627a2d637892ab3c953
itsfarhankhan28 <famannat@gmail.com>
2023-04-08 19:40:49 +0530

    style(Projects): changed style of project section

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -44,7 +44,7 @@ const Projects = () => {
   return (
     <>
     <div className='overflow-hidden bg-custom-brown'>
-      <div className='flex justify-center items-center gap-5 flex-col h-[100vh]'>
+      <div className='flex justify-center items-center gap-5 flex-col h-auto mb-10'>
       <h1 className='font-herofont text-[35px] font-semibold text-center'>My Work</h1>
         <Carousel
           cards={cards}
