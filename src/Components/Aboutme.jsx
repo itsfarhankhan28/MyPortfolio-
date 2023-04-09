02b4dc275a1303bd66c0d2b31252f83e989f259f
itsfarhankhan28 <famannat@gmail.com>
2023-04-09 06:40:28 +0530

    feat(Aboutme): added image and changed style of about me section

--- src/Components/Aboutme.jsx
+++ src/Components/Aboutme.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Aboutmeimg from '../Assets/aboutmeimg.png'
 
 const Aboutme = () => {
   return (
     <>
-        <div className='h-screen bg-green-200'>
-            <h1>About Me</h1>
+        <div className='h-auto bg-green-200 flex flex-col justify-center items-center gap-5'>
+            <h1 className='font-herofont text-[35px] font-semibold'>About Me</h1>
+            <img className='w-[200px]' src={Aboutmeimg} alt="" />
         </div> 
     </>
   )
