2e7c2a39aa9ca77ab96fb02e55221113a05308a5
itsfarhankhan28 <famannat@gmail.com>
2023-04-14 00:52:20 +0530

    feat(Others): added framer motion animation

--- src/Components/Others.jsx
+++ src/Components/Others.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {motion} from 'framer-motion'
 
 const Others = () => {
   return (
@@ -9,7 +10,11 @@ const Others = () => {
                 <div className='flex gap-3'>
                     <img src="https://skillicons.dev/icons?i=figma" alt="" />
                     <div className='relative bg-gray-300 w-[170px] h-7 my-auto rounded-full'>
-                        <div className='absolute inset-0 bg-blue-300 w-[170px] rounded-full'></div>
+                        <motion.div 
+                        initial={{width:0}}
+                        animate={{width:170}}
+                        transition={{duration:1}}
+                        className='absolute inset-0 bg-blue-300 w-[170px] rounded-full'></motion.div>
                     </div>
                     <div className='my-auto'>
                         <h1 className='font-herofont font-bold text-xl'>100%</h1>
@@ -18,7 +23,11 @@ const Others = () => {
                 <div className='flex gap-3'>
                     <img src="https://skillicons.dev/icons?i=webflow" alt="" />
                     <div className='relative bg-gray-300 w-[170px] h-7 my-auto rounded-full'>
-                        <div className='absolute inset-0 bg-blue-300 w-[110px] rounded-l-full'></div>
+                        <motion.div 
+                        initial={{width:0}}
+                        animate={{width:110}}
+                        transition={{duration:1}}
+                        className='absolute inset-0 bg-blue-300 w-[110px] rounded-l-full'></motion.div>
                     </div>
                     <div className='my-auto'>
                         <h1 className='font-herofont font-bold text-xl'>60%</h1>
@@ -27,7 +36,11 @@ const Others = () => {
                 <div className='flex gap-3'>
                     <img src="https://skillicons.dev/icons?i=mui" alt="" />
                     <div className='relative bg-gray-300 w-[170px] h-7 my-auto rounded-full'>
-                        <div className='absolute inset-0 bg-blue-300 w-[135px] rounded-l-full'></div>
+                        <motion.div 
+                        initial={{width:0}}
+                        animate={{width:135}}
+                        transition={{duration:1}}
+                        className='absolute inset-0 bg-blue-300 w-[135px] rounded-l-full'></motion.div>
                     </div>
                     <div className='my-auto'>
                         <h1 className='font-herofont font-bold text-xl'>80%</h1>
@@ -36,7 +49,11 @@ const Others = () => {
                 <div className='flex gap-3'>
                     <img src="https://skillicons.dev/icons?i=markdown" alt="" />
                     <div className='relative bg-gray-300 w-[170px] h-7 my-auto rounded-full'>
-                        <div className='absolute inset-0 bg-blue-300 w-[170px] rounded-full'></div>
+                        <motion.div 
+                        initial={{width:0}}
+                        animate={{width:170}}
+                        transition={{duration:1}}
+                        className='absolute inset-0 bg-blue-300 w-[170px] rounded-full'></motion.div>
                     </div>
                     <div className='my-auto'>
                         <h1 className='font-herofont font-bold text-xl'>100%</h1>
@@ -45,7 +62,11 @@ const Others = () => {
                 <div className='flex gap-3'>
                     <img src="https://skillicons.dev/icons?i=mongodb" alt="" />
                     <div className='relative bg-gray-300 w-[170px] h-7 my-auto rounded-full'>
-                        <div className='absolute inset-0 bg-blue-300 w-[155px] rounded-l-full'></div>
+                        <motion.div 
+                        initial={{width:0}}
+                        animate={{width:155}}
+                        transition={{duration:1}}
+                        className='absolute inset-0 bg-blue-300 w-[155px] rounded-l-full'></motion.div>
                     </div>
                     <div className='my-auto'>
                         <h1 className='font-herofont font-bold text-xl'>95%</h1>
