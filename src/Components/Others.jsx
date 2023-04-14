0e149e4634330254d85a0025d07d02f36cd855f4
itsfarhankhan28 <famannat@gmail.com>
2023-04-14 07:49:37 +0530

    feat(Others): added framer motion inView animation

--- src/Components/Others.jsx
+++ src/Components/Others.jsx
@@ -1,71 +1,153 @@
 import React from 'react'
 import {motion} from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
+import { useEffect } from 'react'
+import { useAnimation } from 'framer-motion'
 
 const Others = () => {
+
+    const [ref , inView] = useInView({
+        threshold:0.2
+    })
+
+    const animateLang1 = useAnimation()
+    const animateLang2 = useAnimation()
+    const animateLang3 = useAnimation()
+    const animateLang4= useAnimation()
+    const animateLang5 = useAnimation()
+
+    useEffect(()=>{
+        if(inView){
+            animateLang1.start({
+                width:170,
+                transition:{
+                    duration:1
+                }
+            })
+        }
+        if(inView){
+            animateLang2.start({
+                width:110,
+                transition:{
+                    duration:1
+                }
+            })
+        }
+        if(inView){
+            animateLang3.start({
+                width:135,
+                transition:{
+                    duration:1
+                }
+            })
+        }
+        if(inView){
+            animateLang4.start({
+                width:170,
+                transition:{
+                    duration:1
+                }
+            })
+        }
+        if(inView){
+            animateLang5.start({
+                width:155,
+                transition:{
+                    duration:1
+                }
+            })
+        }
+        if(!inView){
+            animateLang1.start({
+                width:0
+            })
+        }
+        if(!inView){
+            animateLang2.start({
+                width:0
+            })
+        }
+        if(!inView){
+            animateLang3.start({
+                width:0
+            })
+        }
+        if(!inView){
+            animateLang4.start({
+                width:0
+            })
+        }
+        if(!inView){
+            animateLang5.start({
+                width:0
+            })
+        }
+    },[inView])
+
   return (
     <>
         <div className=''>
             <h1 className='text-center font-herofont text-xl font-semibold'>Others</h1>
-            <div className='px-2 py-10 grid grid-cols-1 gap-8'>
+            <div ref={ref} className='px-2 py-10 grid grid-cols-1 gap-8'>
+
+                {/* Lang1 */}
                 <div className='flex gap-3'>
                     <img src="https://skillicons.dev/icons?i=figma" alt="" />
                     <div className='relative bg-gray-300 w-[170px] h-7 my-auto rounded-full'>
-                        <motion.div 
-                        initial={{width:0}}
-                        animate={{width:170}}
-                        transition={{duration:1}}
+                        <motion.div
+                        animate={animateLang1}
                         className='absolute inset-0 bg-blue-300 w-[170px] rounded-full'></motion.div>
                     </div>
                     <div className='my-auto'>
                         <h1 className='font-herofont font-bold text-xl'>100%</h1>
                     </div>
                 </div>
+
+                {/* Lang2 */}
                 <div className='flex gap-3'>
                     <img src="https://skillicons.dev/icons?i=webflow" alt="" />
                     <div className='relative bg-gray-300 w-[170px] h-7 my-auto rounded-full'>
-                        <motion.div 
-                        initial={{width:0}}
-                        animate={{width:110}}
-                        transition={{duration:1}}
+                        <motion.div
+                        animate={animateLang2}
                         className='absolute inset-0 bg-blue-300 w-[110px] rounded-l-full'></motion.div>
                     </div>
                     <div className='my-auto'>
                         <h1 className='font-herofont font-bold text-xl'>60%</h1>
                     </div>
                 </div>
+
+                {/* Lang3 */}
                 <div className='flex gap-3'>
                     <img src="https://skillicons.dev/icons?i=mui" alt="" />
                     <div className='relative bg-gray-300 w-[170px] h-7 my-auto rounded-full'>
-                        <motion.div 
-                        initial={{width:0}}
-                        animate={{width:135}}
-                        transition={{duration:1}}
+                        <motion.div
+                        animate={animateLang3}
                         className='absolute inset-0 bg-blue-300 w-[135px] rounded-l-full'></motion.div>
                     </div>
                     <div className='my-auto'>
                         <h1 className='font-herofont font-bold text-xl'>80%</h1>
                     </div>
                 </div>
+
+                {/* Lang4 */}
                 <div className='flex gap-3'>
                     <img src="https://skillicons.dev/icons?i=markdown" alt="" />
                     <div className='relative bg-gray-300 w-[170px] h-7 my-auto rounded-full'>
-                        <motion.div 
-                        initial={{width:0}}
-                        animate={{width:170}}
-                        transition={{duration:1}}
+                        <motion.div
+                        animate={animateLang4}
                         className='absolute inset-0 bg-blue-300 w-[170px] rounded-full'></motion.div>
                     </div>
                     <div className='my-auto'>
                         <h1 className='font-herofont font-bold text-xl'>100%</h1>
                     </div>
                 </div>
+
+                {/* Lang5 */}
                 <div className='flex gap-3'>
                     <img src="https://skillicons.dev/icons?i=mongodb" alt="" />
                     <div className='relative bg-gray-300 w-[170px] h-7 my-auto rounded-full'>
-                        <motion.div 
-                        initial={{width:0}}
-                        animate={{width:155}}
-                        transition={{duration:1}}
+                        <motion.div
+                        animate={animateLang5}
                         className='absolute inset-0 bg-blue-300 w-[155px] rounded-l-full'></motion.div>
                     </div>
                     <div className='my-auto'>
