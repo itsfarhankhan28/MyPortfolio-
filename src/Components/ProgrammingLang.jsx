12d82cba071d4ab6a65664fec8d8b6c177c403c1
itsfarhankhan28 <famannat@gmail.com>
2023-04-14 07:51:32 +0530

    feat(ProgrammingLang): added framer motion inView animation

--- src/Components/ProgrammingLang.jsx
+++ src/Components/ProgrammingLang.jsx
@@ -1,21 +1,102 @@
 import React from 'react'
 import {motion} from 'framer-motion'
+import { useInView } from 'react-intersection-observer'
+import { useEffect } from 'react'
+import { useAnimation } from 'framer-motion'
 
 const ProgrammingLang = () => {
+
+    const [ref , inView] = useInView({
+        threshold:0.2
+    })
+
+    const animateLang1 = useAnimation()
+    const animateLang2 = useAnimation()
+    const animateLang3 = useAnimation()
+    const animateLang4= useAnimation()
+    const animateLang5 = useAnimation()
+
+
+    useEffect(()=>{
+        if(inView){
+            animateLang1.start({
+                width:135,
+                transition:{
+                    duration:1
+                }
+            })
+        }
+        if(inView){
+            animateLang2.start({
+                width:150,
+                transition:{
+                    duration:1
+                }
+            })
+        }
+        if(inView){
+            animateLang3.start({
+                width:110,
+                transition:{
+                    duration:1
+                }
+            })
+        }
+        if(inView){
+            animateLang4.start({
+                width:155,
+                transition:{
+                    duration:1
+                }
+            })
+        }
+        if(inView){
+            animateLang5.start({
+                width:100,
+                transition:{
+                    duration:1
+                }
+            })
+        }
+        if(!inView){
+            animateLang1.start({
+                width:0
+            })
+        }
+        if(!inView){
+            animateLang2.start({
+                width:0
+            })
+        }
+        if(!inView){
+            animateLang3.start({
+                width:0
+            })
+        }
+        if(!inView){
+            animateLang4.start({
+                width:0
+            })
+        }
+        if(!inView){
+            animateLang5.start({
+                width:0
+            })
+        }
+    },[inView])
+
   return (
     <>
         <div className=''>
             <h1 className='text-center font-herofont text-xl font-semibold'>Programming Languages</h1>
-            <div className='px-2 py-10 grid grid-cols-1 gap-8'>
+            <div ref={ref} className='px-2 py-10 grid grid-cols-1 gap-8'>
 
                 {/* Lang1 */}
                 <div className='flex gap-3'>
                     <img src="https://skillicons.dev/icons?i=c" alt="" />
                     <div className='relative bg-gray-300 w-[170px] h-7 my-auto rounded-full'>
                         <motion.div 
-                        initial={{width:0}}
-                        animate={{width:135}}
-                        transition={{duration:1}}
+                        animate={animateLang1}
                         className='absolute inset-0 bg-blue-300 w-[135px] rounded-l-full'></motion.div>
                     </div>
                     <div className='my-auto'>
@@ -27,10 +108,8 @@ const ProgrammingLang = () => {
                 <div className="flex gap-3">
                     <img src="https://skillicons.dev/icons?i=python" alt="" />
                     <div className='relative bg-gray-300 w-[170px] h-7 my-auto rounded-full'>
-                        <motion.div 
-                        initial={{width:0}}
-                        animate={{width:150}}
-                        transition={{duration:1}}
+                        <motion.div
+                        animate={animateLang2}
                         className='absolute inset-0 bg-blue-300 w-[150px] rounded-l-full'></motion.div>
                     </div>
                     <div className='my-auto'>
@@ -42,10 +121,8 @@ const ProgrammingLang = () => {
                 <div className="flex gap-3">
                     <img src="https://skillicons.dev/icons?i=java" alt="" />
                     <div className='relative bg-gray-300 w-[170px] h-7 my-auto rounded-full'>
-                        <motion.div 
-                        initial={{width:0}}
-                        animate={{width:110}}
-                        transition={{duration:1}}
+                        <motion.div
+                        animate={animateLang3}
                         className='absolute inset-0 bg-blue-300 w-[110px] rounded-l-full'></motion.div>
                     </div>
                     <div className='my-auto'>
@@ -57,10 +134,8 @@ const ProgrammingLang = () => {
                 <div className="flex gap-3">
                     <img src="https://skillicons.dev/icons?i=javascript" alt="" />
                     <div className='relative bg-gray-300 w-[170px] h-7 my-auto rounded-full'>
-                        <motion.div 
-                        initial={{width:0}}
-                        animate={{width:155}}
-                        transition={{duration:1}}
+                        <motion.div
+                        animate={animateLang4}
                         className='absolute inset-0 bg-blue-300 w-[155px] rounded-l-full'></motion.div>
                     </div>
                     <div className='my-auto'>
@@ -72,10 +147,8 @@ const ProgrammingLang = () => {
                 <div className="flex gap-3">
                     <img src="https://skillicons.dev/icons?i=typescript" alt="" />
                     <div className='relative bg-gray-300 w-[170px] h-7 my-auto rounded-full'>
-                        <motion.div 
-                        initial={{width:0}}
-                        animate={{width:100}}
-                        transition={{duration:1}}
+                        <motion.div
+                        animate={animateLang5}
                         className='absolute inset-0 bg-blue-300 w-[100px] rounded-l-full'></motion.div>
                     </div>
                     <div className='my-auto'>
