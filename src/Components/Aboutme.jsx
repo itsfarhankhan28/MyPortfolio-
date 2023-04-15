73ab3da688693f88913408192188b884d85d7032
itsfarhankhan28 <famannat@gmail.com>
2023-04-15 20:31:07 +0530

    style(Aboutme): changed style of Aboutme section

--- src/Components/Aboutme.jsx
+++ src/Components/Aboutme.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
-import Aboutmeimg from '../Assets/aboutmeimg.png'
+import aboutmeimg from '../Assets/portfolioaboutmeimg.jpg'
 
 const Aboutme = () => {
   return (
     <>
-        <div className='h-auto bg-green-200 flex flex-col justify-center items-center gap-5'>
-            <h1 className='font-herofont text-[35px] font-semibold'>About Me</h1>
-            <img className='w-[200px]' src={Aboutmeimg} alt="" />
-        </div> 
+      <div className='bg-custom-brown flex justify-center items-center h-screen'>
+        <div className='lg:mx-[250px] md:mx-[100px] xxsm:mx-[50px] w-auto flex md:flex-row xxsm:flex-col'>
+        <div className='w-[40%]'>
+          <img className='w-[300px]' src={aboutmeimg} alt="" />
+        </div>
+        <div className='font-herofont w-[60%]'>
+          <h1 className='text-[50px] font-bold'>About Me</h1>
+          <p className='leading-7 mt-3'>Hello there👋 My name is Farhan Khan and I'm aiming to be a web developer 👨‍💻. I am currently pursuing my degree in Information technology and have a passion for all things web development. I have experience working with HTML, CSS, JavaScript, and several web development frameworks such as React etc.</p>
+          <br />
+          <p className='leading-7'>
+          Apart from my academic pursuits, I've also been involved in several personal projects, including developing a responsive spotify clone with complete working backend. These projects have allowed me to further hone my skills and provided me with valuable hands-on experience.
+          </p>
+        </div>
+        </div>
+      </div>
     </>
   )
 }
