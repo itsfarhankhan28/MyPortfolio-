970197400114590484910ef9f2f15ea73d54d429
itsfarhankhan28 <famannat@gmail.com>
2023-04-15 20:54:23 +0530

    style(Aboutme): changed style of Aboutme section

--- src/Components/Aboutme.jsx
+++ src/Components/Aboutme.jsx
@@ -4,13 +4,13 @@ import aboutmeimg from '../Assets/portfolioaboutmeimg.jpg'
 const Aboutme = () => {
   return (
     <>
-      <div className='bg-custom-brown flex justify-center items-center h-screen'>
-        <div className='lg:mx-[250px] md:mx-[100px] xxsm:mx-[50px] w-auto flex md:flex-row xxsm:flex-col'>
+      <div className='bg-custom-brown flex justify-center items-center xxsm:h-auto md:h-screen xxsm:pb-20 md:pb-0'>
+        <div className='lg:mx-[250px] md:mx-[50px] h-auto w-auto flex justify-center items-center lg:text-left xxsm:text-center lg:flex-row xxsm:flex-col'>
         <div className='w-[40%]'>
           <img className='w-[300px]' src={aboutmeimg} alt="" />
         </div>
         <div className='font-herofont w-[60%]'>
-          <h1 className='text-[50px] font-bold'>About Me</h1>
+          <h1 className='md:text-[50px] xxsm:text-[30px] font-bold'>About Me</h1>
           <p className='leading-7 mt-3'>Hello there👋 My name is Farhan Khan and I'm aiming to be a web developer 👨‍💻. I am currently pursuing my degree in Information technology and have a passion for all things web development. I have experience working with HTML, CSS, JavaScript, and several web development frameworks such as React etc.</p>
           <br />
           <p className='leading-7'>
