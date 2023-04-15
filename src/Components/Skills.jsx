73d7e8605dd4cb304c80a3a4476f3a8aa09773b2
itsfarhankhan28 <famannat@gmail.com>
2023-04-15 20:56:03 +0530

    style(Skills): changed style of skill section

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -7,7 +7,7 @@ import Others from './Others'
 const Skills = () => {
   return (
     <>
-     <div className='h-auto bg-custom-brown flex flex-col'>
+     <div className='h-auto xxsm:pb-20 md:pb-0 bg-custom-brown flex flex-col'>
         <h1 className='text-center pt-5 font-herofont text-[35px] font-semibold'>My Skills</h1>
         <div className='w-auto mx-5 h-auto grid lg:grid-cols-4 md:grid-cols-2 xxsm:grid-cols-1 gap-10 mt-5'>
             <ProgrammingLang/>
