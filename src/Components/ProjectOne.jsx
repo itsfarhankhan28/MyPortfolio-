b6ef8b5bec79c83eebbf1f0bc23c762d8e5d8b9d
itsfarhankhan28 <famannat@gmail.com>
2023-05-30 13:13:27 +0530

    feat(ProjectOne): added new animation to ProjectOne

--- src/Components/ProjectOne.jsx
+++ src/Components/ProjectOne.jsx
@@ -36,9 +36,9 @@ const ProjectOne = () => {
       animationOrder.initial,
       animationOrder.fadeInEnd,
       animationOrder.createBranchEnd,
-      animationOrder.endTextFadeInStart
+      // animationOrder.endTextFadeInStart
     ],
-    [0,1,1,0]
+    [0,1,1]
   )
 
   const scale = useTransform(
@@ -46,25 +46,85 @@ const ProjectOne = () => {
     [
       animationOrder.initial,
       animationOrder.fadeInEnd,
-      animationOrder.showLoadingScreenEnd,
-      animationOrder.createBranchStart
+      // animationOrder.showLoadingScreenEnd,
+      // animationOrder.createBranchStart
     ],
-    [3,1,1,0.5]
+    [2,1]
+  )
+
+  const x = useTransform(
+    scrollYProgress,
+    [
+      animationOrder.initial,
+      animationOrder.showParagraphOne,
+      // animationOrder.hideParagraphOne,
+      // animationOrder.showParagraphTwoStart,
+      // animationOrder.showParagraphTwoEnd,
+      // animationOrder.hideParagraphTwo,
+      // animationOrder.showLoadingScreenStart,
+      // animationOrder.showLoadingScreenEnd,
+      // animationOrder.createBranchEnd
+    ],
+    ["0%","10%"]
+  )
+
+  const paragraph1Opacity = useTransform(
+    scrollYProgress,
+    [
+      animationOrder.fadeInEnd + 0.02,
+      animationOrder.showParagraphOne,
+      // animationOrder.hideParagraphOne
+    ],
+    [0,1]
+  )
+
+  const paragraph1TranslateY = useTransform(
+    scrollYProgress,
+    [
+      animationOrder.fadeInEnd + 0.02,
+      animationOrder.showParagraphOne,
+      animationOrder.hideParagraphOne
+    ],
+    ["4rem","0rem","-4rem"]
+  )
+
+  const paragraph2Opacity = useTransform(
+    scrollYProgress,
+    [
+      animationOrder.showParagraphTwoStart,
+      animationOrder.showParagraphTwoEnd,
+      animationOrder.hideParagraphTwo
+    ],
+    [0,1,0]
+  )
+
+  const paragraph2TranslateY = useTransform(
+    scrollYProgress,
+    [
+      animationOrder.showParagraphTwoStart,
+      animationOrder.showParagraphTwoEnd,
+      animationOrder.hideParagraphTwo
+    ],
+    ["4rem","0rem","-4rem"]
+  )
+
+  const position = useTransform(scrollYProgress,(pos)=>
+  pos >= 1 ? "relative" : "fixed"
   )
 
   return (
     <>
-    <div className='bg-Mischka h-screen sticky'>
-        <motion.div style={{opacity , scale}} ref={targetRef} className='flex w-[1000px] mx-auto pt-16 leading-[60px] z-20'>
-        <p className='w-[50%] font-herofont my-auto'>
+    <section className='bg-Mischka h-[100vh] sticky'>
+        <motion.div ref={targetRef} className='flex justify-center items-center w-[1000px] mx-auto pt-16 leading-[60px] z-20'>
+        <motion.p style={{opacity:paragraph1Opacity,y:paragraph1TranslateY}} className='w-[50%] font-herofont pt-20'>
             <h1 className='text-[40px]'>Project 01</h1>
             <h1 className='text-[55px] font-semibold'>All About Recipe</h1>
             <p className='leading-[30px] font-semibold'>The purpose of this website <br /> is to provide users with a <br />
             collection of recipes ,<br /> cooking tips and food <br /> related information . The <br /> goal is to inspire and assist <br /> users in the kitchen . The <br /> technology used is MERN stack</p>
-        </p>
-        <img className='w-[50%] my-auto' src={projectimg1} alt="" />
+        </motion.p>
+        <motion.img style={{opacity , scale , x}}  className='w-[50%] my-auto' src={projectimg1} alt="" />
         </motion.div>
-    </div>  
+    </section>  
     </>
   )
 }
