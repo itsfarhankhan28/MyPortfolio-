42c3997685c2b9bb8f2a2c532c2897ba5c57ff45
itsfarhankhan28 <famannat@gmail.com>
2023-06-07 14:31:20 +0530

    feat(Aboutme): changed style and content of About me page

--- src/Components/Aboutme.jsx
+++ src/Components/Aboutme.jsx
@@ -1,105 +1,32 @@
 import React from 'react'
-import aboutmeimg from '../Assets/portfolioaboutmeimg.jpg'
-import {motion} from 'framer-motion'
+import { motion , useTransform , useScroll } from 'framer-motion'
+import { useRef } from 'react'
 
 const Aboutme = () => {
 
-  const typingContainer = {
-    hidden:{
-      opacity:0,
-    },
-    show:{
-      opacity:1,
-      transition:{
-        staggerChildren:0.4,
-        ease:'easeInOut'
-      }
-    }
-  }
+  const targetRef = useRef()
+    const {scrollYProgress} = useScroll({
+    target:targetRef,
+    offset:["start end","end start"]
+    })
 
-  const typingText = {
-    hidden:{
-      opacity:0,
-      y:'-20px'
-    },
-    show:{
-      opacity:1,
-      y:0,
-      transition:{
-        ease:'easeInOut'
-      }
-    }
-  }
+    const scale = useTransform(scrollYProgress , [0.1,0.4] , [1,2.5])
+    const x = useTransform(
+    scrollYProgress,
+    [0.1,0.25,0.7,1],
+    ["0%","0%","0%","-80%"]
+    )
 
-  const animatepara = {
-    hidden:{
-      opacity:0,
-      y:'-20px'
-    },
-    show:{
-      opacity:1,
-      y:0,
-      transition:{
-        ease:'easeInOut'
-      }
-    }
-  }
-
-  const animateImage = {
-    hidden:{
-      opacity:0,
-      y:100,
-      width:80
-    },
-    show:{
-      width:300,
-      opacity:1,
-      y:0,
-      transition:{
-        ease:"easeInOut",
-      }
-    }
-  }
+    const opacity = useTransform(scrollYProgress , [0.5,1],[1,0])
 
   return (
     <>
-      <div className='bg-Mischka flex justify-center items-center xxsm:h-auto md:h-auto xxsm:pb-20 md:pb-0'>
-        <div className='mx-auto w-[900px] gap-5 h-screen flex justify-center items-center lg:text-left xxsm:text-center lg:flex-row xxsm:flex-col'>
-        <div className='w-[40%]'>
-          <motion.img
-          variants={animateImage}
-          animate="show"
-          initial="hidden" 
-          className='w-[300px]' src={aboutmeimg} alt="" />
-        </div>
-        <div className='font-herofont w-[60%]'>
-          <motion.h1 
-          variants={typingContainer} 
-          animate="show" 
-          initial="hidden" 
-          className='md:text-[50px] xxsm:text-[30px] font-bold'>
-            {
-              Array.from('About Me').map((word  , i)=>(
-                <motion.span variants={typingText} key={i}>{word}</motion.span>
-              ))
-            }
-          </motion.h1>
-          <motion.p 
-          variants={animatepara} 
-          animate="show" 
-          initial="hidden" 
-          className='leading-7 mt-1'>Hello there👋 My name is Farhan Khan and I'm aiming to be a web developer 👨‍💻. I am currently pursuing my degree in Information technology and have a passion for all things web development. I have experience working with HTML, CSS, JavaScript, and several web development frameworks such as React etc.</motion.p>
-          <br />
-          <motion.p
-          variants={animatepara} 
-          animate="show" 
-          initial="hidden" 
-          className='leading-7'>
-          Apart from my academic pursuits, I've also been involved in several personal projects, including developing a responsive spotify clone with complete working backend. These projects have allowed me to further hone my skills and provided me with valuable hands-on experience.
-          </motion.p>
-        </div>
-        </div>
-      </div>
+    <div className='h-[100vh] overflow-hidden flex justify-center items-center bg-Mischka '>
+      <motion.h1
+      style={{scale , x , opacity}} 
+      ref={targetRef} 
+      className='z-10 font-herofont text-[100px] font-semibold'>About Me</motion.h1>
+    </div>   
     </>
   )
 }
