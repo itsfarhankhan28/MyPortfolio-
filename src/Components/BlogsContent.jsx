62ad269493c7e554e05eddf5c9f49bef207f752c
itsfarhankhan28 <famannat@gmail.com>
2023-06-07 20:29:37 +0530

    feat(BlogsContent): created blogs content

---
+++ src/Components/BlogsContent.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import arrow from '../Assets/arrow.png'
+import heart from '../Assets/heart.png'
+import eye from '../Assets/eye.png'
+
+const BlogsContent = () => {
+
+  return (
+    <>
+    <div className='h-[80vh] bg-Mischka'>
+        <div className='w-[1000px] mx-auto p-10 flex gap-16'>
+            <div className='w-[400px] h-[240px] shadow-2xl rounded-3xl p-5' >
+                <h1 className='font-herofont text-[25px]'>Blog 01</h1>
+                <p className='text-[30px] font-herofont font-bold'>User authentication in MERN stack(Backend)</p>
+                <div className='flex gap-5'>
+                    <div className='mt-2 flex items-center gap-1'>
+                        <img className='h-[20px]' src={heart} alt="" />
+                        <h1 className='font-bold text-xl'>7</h1>
+                    </div>
+                    <div className='mt-2 flex items-center gap-1'>
+                        <img className='h-[20px]' src={eye} alt="" />
+                        <h1 className='font-bold text-xl'>2007</h1>
+                    </div>
+                </div>
+                <div className='flex items-center gap-1 hover:gap-2 mt-3'>
+                    <button className='font-herofont font-bold text-xl'><a href="https://dev.to/itsfarhankhan28/user-authentication-in-mern-stack-part-1-4bj3">Visit Blog</a></button>
+                    <img className='w-[25px] h-[15px]' src={arrow} alt="" />
+                </div>
+            </div>
+
+
+
+            <div className='w-[400px] h-[240px] shadow-2xl rounded-3xl p-5 z-30' >
+                <h1 className='font-herofont text-[25px]'>Blog 02</h1>
+                <p className='text-[30px] font-herofont font-bold'>User authentication in MERN stack(Frontend)</p>
+                <div className='flex gap-5'>
+                    <div className='mt-2 flex items-center gap-1'>
+                        <img className='h-[20px]' src={heart} alt="" />
+                        <h1 className='font-bold text-xl'>5</h1>
+                    </div>
+                    <div className='mt-2 flex items-center gap-1'>
+                        <img className='h-[20px]' src={eye} alt="" />
+                        <h1 className='font-bold text-xl'>55</h1>
+                    </div>
+                </div>
+                <div className='flex items-center gap-1 hover:gap-2 mt-3'>
+                    <button className='font-herofont font-bold text-xl'><a href="https://dev.to/itsfarhankhan28/user-authentication-in-mern-stack-frontend-1c14">Visit Blog</a></button>
+                    <img className='w-[25px] h-[15px]' src={arrow} alt="" />
+                </div>
+            </div>
+        </div>
+    </div>  
+    </>
+  )
+}
+
+export default BlogsContent
