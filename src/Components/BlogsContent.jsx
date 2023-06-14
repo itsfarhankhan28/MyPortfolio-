5dbe339283b9d23149ae44f7815a09db35436935
itsfarhankhan28 <famannat@gmail.com>
2023-06-14 21:01:10 +0530

    feat(BlogsContent): added new blog

--- src/Components/BlogsContent.jsx
+++ src/Components/BlogsContent.jsx
@@ -7,9 +7,9 @@ const BlogsContent = () => {
 
   return (
     <>
-    <div className='h-[80vh] bg-Mischka'>
-        <div className='w-[1000px] mx-auto p-10 flex gap-16'>
-            <div className='w-[400px] h-[240px] shadow-2xl rounded-3xl p-5' >
+    <div className='h-auto bg-Mischka'>
+        <div className='w-[1000px] mx-auto p-10 flex flex-wrap gap-16'>
+            <div className='w-[400px] h-auto shadow-2xl rounded-3xl p-5' >
                 <h1 className='font-herofont text-[25px]'>Blog 01</h1>
                 <p className='text-[30px] font-herofont font-bold'>User authentication in MERN stack(Backend)</p>
                 <div className='flex gap-5'>
@@ -19,7 +19,7 @@ const BlogsContent = () => {
                     </div>
                     <div className='mt-2 flex items-center gap-1'>
                         <img className='h-[20px]' src={eye} alt="" />
-                        <h1 className='font-bold text-xl'>2007</h1>
+                        <h1 className='font-bold text-xl'>2215</h1>
                     </div>
                 </div>
                 <div className='flex items-center gap-1 hover:gap-2 mt-3'>
@@ -30,7 +30,7 @@ const BlogsContent = () => {
 
 
 
-            <div className='w-[400px] h-[240px] shadow-2xl rounded-3xl p-5 z-30' >
+            <div className='w-[400px] h-auto shadow-2xl rounded-3xl p-5 z-30' >
                 <h1 className='font-herofont text-[25px]'>Blog 02</h1>
                 <p className='text-[30px] font-herofont font-bold'>User authentication in MERN stack(Frontend)</p>
                 <div className='flex gap-5'>
@@ -40,11 +40,30 @@ const BlogsContent = () => {
                     </div>
                     <div className='mt-2 flex items-center gap-1'>
                         <img className='h-[20px]' src={eye} alt="" />
-                        <h1 className='font-bold text-xl'>55</h1>
+                        <h1 className='font-bold text-xl'>93</h1>
                     </div>
                 </div>
                 <div className='flex items-center gap-1 hover:gap-2 mt-3'>
                     <button className='font-herofont font-bold text-xl'><a href="https://dev.to/itsfarhankhan28/user-authentication-in-mern-stack-frontend-1c14">Visit Blog</a></button>
+                    <img className='w-[25px] h-[15px]' src={arrow} alt="" />
+                </div>
+            </div>
+
+            <div className='w-[400px] h-auto shadow-2xl rounded-3xl p-5 z-30' >
+                <h1 className='font-herofont text-[25px]'>Blog 03</h1>
+                <p className='text-[30px] font-herofont font-bold'>User authentication in MERN stack(Frontend and Backend connection)</p>
+                <div className='flex gap-5'>
+                    <div className='mt-2 flex items-center gap-1'>
+                        <img className='h-[20px]' src={heart} alt="" />
+                        <h1 className='font-bold text-xl'>1</h1>
+                    </div>
+                    <div className='mt-2 flex items-center gap-1'>
+                        <img className='h-[20px]' src={eye} alt="" />
+                        <h1 className='font-bold text-xl'>25</h1>
+                    </div>
+                </div>
+                <div className='flex items-center gap-1 hover:gap-2 mt-3'>
+                    <button className='font-herofont font-bold text-xl'><a href="https://dev.to/itsfarhankhan28/user-authentication-in-mern-stack-frontend-and-backend-connection-5fjb">Visit Blog</a></button>
                     <img className='w-[25px] h-[15px]' src={arrow} alt="" />
                 </div>
             </div>
