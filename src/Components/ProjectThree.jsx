4db300688dca751a39b9eed803bab9ef9212e976
itsfarhankhan28 <famannat@gmail.com>
2023-06-15 19:10:57 +0530

    style(ProjectThree): changed style

--- src/Components/ProjectThree.jsx
+++ src/Components/ProjectThree.jsx
@@ -84,13 +84,13 @@ const ProjectThree = () => {
     <>
      <section className='bg-Mischka h-[100vh]'>
         <motion.div ref={targetRef} className='flex lg:w-[1000px] md:w-[700px] xxsm:w-[350px] xxsm:text-center lg:text-left lg:flex-row xxsm:flex-col-reverse justify-center items-center mx-auto pt-16 md:leading-[60px] z-20'>
-        <motion.p style={{opacity:paragraph1Opacity,y:paragraph1TranslateY}} className='w-[50%] font-herofont pt-20'>
+        <motion.p style={{opacity:paragraph1Opacity,y:paragraph1TranslateY,width:'fit-content'}} className='w-[50%] font-herofont pt-20'>
             <h1 className='md:text-[35px] lg:text-[40px] xxsm:text-[25px]'>Project 03</h1>
             <h1 className='md:text-[40px] lg:text-[55px] xxsm:text-[35px] font-semibold'>My Blog Web</h1>
             <p className='leading-[30px] font-semibold'>The purpose of this website is to provide users with a collection of recipes ,cooking tips and food related information . The goal is to inspire and assist users in the kitchen . The technology used is MERN stack</p>
             <button className='border border-black md:px-8 mt-3 py-4 font-semibold rounded-xl'>View Source Code</button>
         </motion.p>
-        <motion.img style={{opacity , scale , x}}  className='w-[50%] my-auto' src={project3img} alt="" />
+        <motion.img style={{opacity , scale , x , width:'fit-content'}}  className='w-[50%] my-auto' src={project3img} alt="" />
         </motion.div>
     </section>   
     </>
