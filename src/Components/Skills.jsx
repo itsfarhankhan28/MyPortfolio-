9ac9443b9e91637c4f3e362305af34533e4bb17d
itsfarhankhan28 <famannat@gmail.com>
2023-06-15 17:19:07 +0530

    style(Skills): changed style

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -15,7 +15,7 @@ const Skills = () => {
 
   return (
     <>
-    <div className='h-[100vh] bg-Mischka'>
+    <div className='h-screen bg-Mischka'>
       <motion.div 
       style={{opacity , scale}}
       ref={targetRef}
@@ -26,69 +26,69 @@ const Skills = () => {
       delay:6
         }}}
         className='flex flex-wrap w-full justify-center items-center font-herofont font-semibold'>
-          <div className='m-8 text-center'>
-            <img className='w-[100px]' src="https://skillicons.dev/icons?i=html" alt="" />
-            <h1>HTML</h1>
+          <div className='m-8 text-center flex flex-col gap-1 items-centers justify-center'>
+            <img className='xxsm:w-[60px] md:w-[100px]' src="https://skillicons.dev/icons?i=html" alt="" />
+            <h1 className='xxsm:text-[10px] md:text-[15px]'>HTML</h1>
           </div>
-          <div className='m-8 text-center'>
-            <img className='w-[100px]' src="https://skillicons.dev/icons?i=css" alt="" />
-            <h1>CSS</h1>
+          <div className='m-8 text-center flex flex-col gap-1 items-centers justify-center'>
+            <img className='xxsm:w-[60px] md:w-[100px]' src="https://skillicons.dev/icons?i=css" alt="" />
+            <h1 className='xxsm:text-[10px] md:text-[15px]'>CSS</h1>
           </div>
-          <div className='m-8 text-center'>
-            <img className='w-[100px]' src="https://skillicons.dev/icons?i=javascript" alt="" />
-            <h1>JAVASCRIPT</h1>
+          <div className='m-8 text-center flex flex-col gap-1 items-centers justify-center'>
+            <img className='xxsm:w-[60px] md:w-[100px]' src="https://skillicons.dev/icons?i=javascript" alt="" />
+            <h1 className='xxsm:text-[10px] md:text-[15px]'>JAVASCRIPT</h1>
           </div>
-          <div className='m-8 text-center'>
-            <img className='w-[100px]' src="https://skillicons.dev/icons?i=react" alt="" />
-            <h1>REACT</h1>
+          <div className='m-8 text-center flex flex-col gap-1 items-centers justify-center'>
+            <img className='xxsm:w-[60px] md:w-[100px]' src="https://skillicons.dev/icons?i=react" alt="" />
+            <h1 className='xxsm:text-[10px] md:text-[15px]'>REACT</h1>
           </div>
-          <div className='m-8 text-center'>
-            <img className='w-[100px]' src="https://skillicons.dev/icons?i=nodejs" alt="" />
-            <h1>NODE.JS</h1>
+          <div className='m-8 text-center flex flex-col gap-1 items-centers justify-center'>
+            <img className='xxsm:w-[60px] md:w-[100px]' src="https://skillicons.dev/icons?i=nodejs" alt="" />
+            <h1 className='xxsm:text-[10px] md:text-[15px]'>NODE.JS</h1>
           </div>
-          <div className='m-8 text-center'>
-            <img className='w-[100px]' src="https://skillicons.dev/icons?i=express" alt="" />
-            <h1>EXPRESS.JS</h1>
+          <div className='m-8 text-center flex flex-col gap-1 items-centers justify-center'>
+            <img className='xxsm:w-[60px] md:w-[100px]' src="https://skillicons.dev/icons?i=express" alt="" />
+            <h1 className='xxsm:text-[10px] md:text-[15px]'>EXPRESS.JS</h1>
           </div>
-          <div className='m-8 text-center'>
-            <img className='w-[100px]' src="https://skillicons.dev/icons?i=mongodb" alt="" />
-            <h1>MONGODB</h1>
+          <div className='m-8 text-center flex flex-col gap-1 items-centers justify-center'>
+            <img className='xxsm:w-[60px] md:w-[100px]' src="https://skillicons.dev/icons?i=mongodb" alt="" />
+            <h1 className='xxsm:text-[10px] md:text-[15px]'>MONGODB</h1>
           </div>
-          <div className='m-8 text-center'>
-            <img className='w-[100px]' src="https://skillicons.dev/icons?i=markdown" alt="" />
-            <h1>MARKDOWN</h1>
+          <div className='m-8 text-center flex flex-col gap-1 items-centers justify-center'>
+            <img className='xxsm:w-[60px] md:w-[100px]' src="https://skillicons.dev/icons?i=markdown" alt="" />
+            <h1 className='xxsm:text-[10px] md:text-[15px]'>MARKDOWN</h1>
           </div>
-          <div className='m-8 text-center'>
-            <img className='w-[100px]' src="https://skillicons.dev/icons?i=typescript" alt="" />
-            <h1>TYPESCRIPT</h1>
+          <div className='m-8 text-center flex flex-col gap-1 items-centers justify-center'>
+            <img className='xxsm:w-[60px] md:w-[100px]' src="https://skillicons.dev/icons?i=typescript" alt="" />
+            <h1 className='xxsm:text-[10px] md:text-[15px]'>TYPESCRIPT</h1>
           </div>
-          <div className='m-8 text-center'>
-            <img className='w-[100px]' src="https://skillicons.dev/icons?i=vercel" alt="" />
-            <h1>VERCEL</h1>
+          <div className='m-8 text-center flex flex-col gap-1 items-centers justify-center'>
+            <img className='xxsm:w-[60px] md:w-[100px]' src="https://skillicons.dev/icons?i=vercel" alt="" />
+            <h1 className='xxsm:text-[10px] md:text-[15px]'>VERCEL</h1>
           </div>
-          <div className='m-8 text-center'>
-            <img className='w-[100px]' src="https://skillicons.dev/icons?i=bootstrap" alt="" />
-            <h1>BOOTSTRAP</h1>
+          <div className='m-8 text-center flex flex-col gap-1 items-centers justify-center'>
+            <img className='xxsm:w-[60px] md:w-[100px]' src="https://skillicons.dev/icons?i=bootstrap" alt="" />
+            <h1 className='xxsm:text-[10px] md:text-[15px]'>BOOTSTRAP</h1>
           </div>
-          <div className='m-8 text-center'>
-            <img className='w-[100px]' src="https://skillicons.dev/icons?i=mui" alt="" />
-            <h1>MATERIAL UI</h1>
+          <div className='m-8 text-center flex flex-col gap-1 items-centers justify-center'>
+            <img className='xxsm:w-[60px] md:w-[100px]' src="https://skillicons.dev/icons?i=mui" alt="" />
+            <h1 className='xxsm:text-[10px] md:text-[15px]'>MATERIAL UI</h1>
           </div>
-          <div className='m-8 text-center'>
-            <img className='w-[100px]' src="https://skillicons.dev/icons?i=tailwindcss" alt="" />
-            <h1>TAILWAIND CSS</h1>
+          <div className='m-8 text-center flex flex-col gap-1 items-centers justify-center'>
+            <img className='xxsm:w-[60px] md:w-[100px]' src="https://skillicons.dev/icons?i=tailwindcss" alt="" />
+            <h1 className='xxsm:text-[10px] md:text-[15px]'>TAILWAIND CSS</h1>
           </div>
-          <div className='m-8 text-center'>
-            <img className='w-[100px]' src="https://skillicons.dev/icons?i=postman" alt="" />
-            <h1>POSTMAN</h1>
+          <div className='m-8 text-center flex flex-col gap-1 items-centers justify-center'>
+            <img className='xxsm:w-[60px] md:w-[100px]' src="https://skillicons.dev/icons?i=postman" alt="" />
+            <h1 className='xxsm:text-[10px] md:text-[15px]'>POSTMAN</h1>
           </div>
-          <div className='m-8 text-center'>
-            <img className='w-[100px]' src="https://skillicons.dev/icons?i=figma" alt="" />
-            <h1>FIGMA</h1>
+          <div className='m-8 text-center flex flex-col gap-1 items-centers justify-center'>
+            <img className='xxsm:w-[60px] md:w-[100px]' src="https://skillicons.dev/icons?i=figma" alt="" />
+            <h1 className='xxsm:text-[10px] md:text-[15px]'>FIGMA</h1>
           </div>
-          <div className='m-8 text-center'>
-            <img className='w-[100px]' src="https://skillicons.dev/icons?i=webflow" alt="" />
-            <h1>WEBFLOW</h1>
+          <div className='m-8 text-center flex flex-col gap-1 items-centers justify-center'>
+            <img className='xxsm:w-[60px] md:w-[100px]' src="https://skillicons.dev/icons?i=webflow" alt="" />
+            <h1 className='xxsm:text-[10px] md:text-[15px]'>WEBFLOW</h1>
           </div>
         </motion.div>
     </div>  
